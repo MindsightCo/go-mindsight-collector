ac64b3c55f4fb851811dd5e6ff9d4ead0618f47b
Eddy Reyes <ereyes01@users.noreply.github.com>
2018-06-18 14:55:12 -0500

    Clean up the stack sampler to use context, avoid recursion, use panicparser to parse stack traces

--- main.go
+++ main.go
@@ -1,24 +1,52 @@
 package main
 
 import (
+	"bytes"
+	"context"
 	"fmt"
+	"log"
 	"runtime"
 	"time"
+
+	"github.com/maruel/panicparse/stack"
 )
 
-func printLoop() {
-	time.Sleep(10 * time.Millisecond)
+type nullWriter struct{}
 
+func (w nullWriter) Write(buf []byte) (int, error) {
+	return len(buf), nil
+}
+
+func sampleLoop(ctx context.Context) {
 	buf := make([]byte, 1<<16)
-	runtime.Stack(buf, true)
-	fmt.Printf("%s", buf)
 
-	printLoop()
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-time.After(10 * time.Millisecond):
+			// take a stack sample
+		}
+
+		runtime.Stack(buf, true)
+		stackCtx, err := stack.ParseDump(bytes.NewBuffer(buf), nullWriter{}, true)
+		if err != nil {
+			log.Println("parse stack error:", err)
+		}
+
+		fmt.Println(len(stackCtx.Goroutines))
+	}
+}
+
+func StartMindsightCollector(ctx context.Context) {
+	go sampleLoop(ctx)
 }
 
 func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Millisecond)
+	defer cancel()
 
-	go printLoop()
+	StartMindsightCollector(ctx)
 
 	fmt.Scanln()
 }
